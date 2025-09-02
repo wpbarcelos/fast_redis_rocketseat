77b0c7cf179637a89de347894dd912350e2822fb
Wanderson P. Barcelos <wp.barcelos@gmail.com>
2025-09-02 12:32:42 -0300

    feat: implementing products routes

--- src/main/routes/products_routes.py
+++ src/main/routes/products_routes.py
@@ -1,13 +1,24 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from src.http_types.http_request import HttpRequest
+from src.main.composer.product_creator_composer import product_creator_composer
+from src.main.composer.product_finder_composer import product_finder_composer
 
 products_route_bp = Blueprint("products_routes", __name__)
 
 
 @products_route_bp.route('/products', methods=['POST'])
 def insert_product():
-    return jsonify({"msg": "produto cadastrado"}), 201
+    http_request = HttpRequest(body=request.json)
+    product_creator = product_creator_composer()
+    http_response = product_creator.create(http_request)
+
+    return jsonify(http_response.body), http_response.status_code
 
 
 @products_route_bp.route('/products/<product_name>', methods=['GET'])
 def search_product(product_name: str):
-    return jsonify({"msg": f'produto {product_name}'}), 200
+    http_request = HttpRequest(params={"product_name": product_name})
+    product_finder = product_finder_composer()
+    http_response = product_finder.find_by_name(http_request)
+
+    return jsonify(http_response.body), http_response.status_code
